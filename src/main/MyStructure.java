4ae1c51ed8e1c804feffa2fb2e47dff5de1d7cec
enigma420 <enigma3420@gmail.com>
2020-03-10 22:46:01 +0100

    Create Node class and implement override methods in MyStructure and Node with null return

--- src/main/MyStructure.java
+++ src/main/MyStructure.java
@@ -4,4 +4,19 @@ import java.util.List;
 
 public class MyStructure implements IMyStructure {
     private List<INode> nodes;
+
+    @Override
+    public INode findByCode(String code) {
+        return null;
+    }
+
+    @Override
+    public INode findByRenderer(String renderer) {
+        return null;
+    }
+
+    @Override
+    public int count() {
+        return 0;
+    }
 }
---
+++ src/main/Node.java
@@ -0,0 +1,29 @@
+package main;
+
+public class Node implements INode {
+
+    @Override
+    public String getCode() {
+        return null;
+    }
+
+    @Override
+    public String getRenderer() {
+        return null;
+    }
+
+    @Override
+    public int count() {
+        return 0;
+    }
+
+    @Override
+    public INode findByRenderer(String renderer) {
+        return null;
+    }
+
+    @Override
+    public INode findByCode(String code) {
+        return null;
+    }
+}
