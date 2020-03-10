ca1b5c9e7c6c3fd2b7ebe8b9fadc0df8b5535d5c
enigma420 <enigma3420@gmail.com>
2020-03-10 22:51:07 +0100

    Create Node attributes and constructor, CompositeNode Class which extends Node class and complete Override methods and constructor.

--- src/main/Node.java
+++ src/main/Node.java
@@ -2,14 +2,22 @@ package main;
 
 public class Node implements INode {
 
+    private final String code;
+    private final String renderer;
+
+    public Node(String code, String renderer) {
+        this.code = code;
+        this.renderer = renderer;
+    }
+
     @Override
     public String getCode() {
-        return null;
+        return code;
     }
 
     @Override
     public String getRenderer() {
-        return null;
+        return renderer;
     }
 
     @Override
